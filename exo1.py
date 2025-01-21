af11f62ac0eeefd5a7f17e6912507b2f5f4a3ff2
antoine.pelletier <antoine-3.pelletier@polymntlus.ca>
2025-01-21 16:41:55 -0500

    ex1 complet

--- exo1.py
+++ exo1.py
@@ -1,8 +1,17 @@
 # Demander le nom complet de l'utilisateur
 # Demander l'âge de l'utilisateur
-#allo2
+
+nom = input("Veuillez entrer votre nom complet :")
+age = int(input("Veuillez entrer votre age :"))
+
 # Définir l'année actuelle
 # Calculer l'année de naissance
 
+annee = 2025
+anneeDeNaissance = annee - age
+
 # Afficher un message de bienvenue avec le nom complet
 # Afficher l'année de naissance
+
+print("Bonjour", nom)
+print("Vous etes ne(e) en ", anneeDeNaissance)
