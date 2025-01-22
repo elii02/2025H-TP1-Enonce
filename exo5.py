8134bf074508ddf653cc836b6c50cbd24ae000b5
antoine.pelletier <antoine-3.pelletier@polymntlus.ca>
2025-01-21 20:53:21 -0500

    exo5 fait

--- exo5.py
+++ exo5.py
@@ -6,13 +6,24 @@ g = 9.8
 # Demander la vitesse initiale de la boule
 # Demander la vitesse initiale de la boule
 
+vi = float(input("Veuillez saisir la vitesse initiale de la boule (en m/s) : "))
+
 # Demander l'angle de lancement
+
+angle = float(input("Veuillez saisir l'angle de lancement (en degrés) : "))
 
 # Convertir l'angle en radians
 
+radians = angle * math.pi / 180
+
 # Calculer la distance maximale en x
 
+Distance = round((vi ** 2 * math.sin(2 * radians)) / 9.8, 2)
+
 # Afficher la distance maximale arrondie à 2 chiffres après la virgule
+
+print("Pour une vitesse initiale de ", vi, " m/s et un angle de ", angle, " degrés")
+print("La distance parcourue serait de ", Distance, " m")
 
 # Exemple:
 # Pour une vitesse initiale de 10 m/s et un angle de 45 degrés
