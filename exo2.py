60787b172efba0442eec0f5a83327e24246c1a10
Elisabeth KJ <elisabeth.kerr-jarrold@polymtl.ca>
2025-01-21 20:15:55 -0500

    exo2 completed w/ french comments

--- exo2.py
+++ exo2.py
@@ -3,9 +3,9 @@ battery_level = int(input("Entrez le niveau de batterie actuel: "))
 
 # Vérifiez si le niveau de charge est valide
 if battery_level >= 0 and battery_level <= 100:
-    print("Battery level accepted")
+    print("Niveau de batterie accepté")
 else:
-    print("Enter a correct level of battery")
+    print("Entrez un niveau de batterie entre 0 et 100 inclusivement")
 
 # Arrondir le pourcentage à la dizaine la plus proche
 battery_level = round(battery_level/10)*10
@@ -13,12 +13,12 @@ battery_level = round(battery_level/10)*10
 # Calculer le nombre de barres
 barre = "❚"
 nbr_barres = int(battery_level/10)
+
+# Afficher la représentation graphique de la charge de la batterie
 espace = " "
 nbr_espaces = 10-nbr_barres
 
 print("[{0}{1}]".format(nbr_barres*barre, nbr_espaces*espace))
-
-# Afficher la représentation graphique de la charge de la batterie
 
 # Exemple d'utilisation :
 # Si l'utilisateur entre 76, la sortie sera :
